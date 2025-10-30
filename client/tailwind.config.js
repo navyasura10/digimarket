4f77f78aab2d3ed6abfad1f66e3a3b1351f83c8a
navyasura10 <navyasaura10@gmail.com>
2025-10-30 20:59:16 +0530

    Add files via upload

---
+++ client/tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  darkMode: 'class', // ✅ Enable dark mode using class
+  content: [
+    "./index.html",
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
